d90f7cc006d68ff3a15d34a2afca7cbd1ede0f2c
andrew morton <drewish@katherinehouse.com>
2014-09-01 14:20:08 -0700

    Use angular-moment for relative estimates

--- app/js/app.js
+++ app/js/app.js
@@ -6,11 +6,5 @@ var howLongApp = angular.module('howLongApp', [
   'howLongControllers',
   // 'howLongFilters',
   // 'howLongServices'
-]);
-
-// Work around Jekyll's liquid tags.
-howLongApp.config([
-  '$interpolateProvider', function($interpolateProvider) {
-    return $interpolateProvider.startSymbol('{(').endSymbol(')}');
-  }
+  'angularMoment',
 ]);
